bceee49dd9a6e7b60dc5a1ccd45a9be423b5633b
rebeca <rebecabarella13@gmail.com>
2026-06-16 16:51:05 -0300

    adiciona reação a curtida

--- Modelos/Postagem.cs
+++ Modelos/Postagem.cs
@@ -1,10 +1,36 @@
-﻿namespace CRUD.Modelos;
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
-public class Postagem
+namespace CRUD.Modelos;
+
+public class Postagem : INotifyPropertyChanged
+    
 {
     public int Id { get; set; }
     public string Conteudo { get; set; }
     public int Curtidas { get; set; }
     public DateTime Postado_em { get; set; }
     public Usuario Usuario { get; set; }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private bool _foiCurtido;
+    
+    public bool FoiCurtido
+    {
+        get => _foiCurtido;
+        set
+        {
+            if (_foiCurtido != value)
+            {
+                _foiCurtido = value ;
+                NotificarPropriedadeAlterada();
+            }
+        }
+    }
+
+    private void NotificarPropriedadeAlterada([CallerMemberName] string namePropriedade = "")
+    {
+        PropertyChanged?.Invoke(sender:this, new PropertyChangedEventArgs(namePropriedade));
+    }
 }
